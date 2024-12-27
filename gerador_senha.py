e0051f3fbc96cb94164168fba56d8981f5d236bd
sadak2578 <108498678+sadak2578@users.noreply.github.com>
2024-12-27 15:34:09 -0300

    Add files via upload

---
+++ gerador_senha.py
@@ -0,0 +1,20 @@
+import random
+import string
+
+keys_list = ['Irlandadonorte','Valfenda','','Lancelot','Hambster','Navarro']  
+symbols_for_pass = ['!','@','*']
+numbers = ['1','2','3','4','5','6','7','8','9','10','99','28','76','23','1']
+def results(tamanho=20):
+    random_item = random.choice([item for item in keys_list if item])
+    random_item_symbol = random.choice(symbols_for_pass)
+    random_numbers = random.choice(numbers)
+    result = random_item + random_item_symbol + random_numbers
+    
+    others_caracters = tamanho - len(result)
+    
+    if others_caracters > 0:
+        result += ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(others_caracters))
+        
+    print('senha gerada: ', result)
+
+results()
