add4f89638e753a23bbca63dbfae585b65f90bba
sadak2578 <108498678+sadak2578@users.noreply.github.com>
2024-12-27 15:42:57 -0300

    gerador_senha.py

--- gerador_senha.py
+++ gerador_senha.py
@@ -1,10 +1,16 @@
 import random
 import string
 
-keys_list = ['Irlandadonorte','Valfenda','','Lancelot','Hambster','Navarro']  
+keys_list = [
+    'Irlandadonorte','Valfenda','','Lancelot','Hambster','Navarro', 'Adventure','Watermelon','Enchantment','Revolution','Connection','Confidence','Recreation',
+    'Leadership','Commitment','Forgiveness','Awareness','Excitement','Elevation','Exploration','Protection','Motivation','Reflection','Mastermind','Brightness','Difference','Overcoming','RadiantSky',
+    'Transformation','Stargazing'
+
+
+]  
 symbols_for_pass = ['!','@','*']
 numbers = ['1','2','3','4','5','6','7','8','9','10','99','28','76','23','1']
-def results(tamanho=20):
+def results(tamanho=21):
     random_item = random.choice([item for item in keys_list if item])
     random_item_symbol = random.choice(symbols_for_pass)
     random_numbers = random.choice(numbers)
